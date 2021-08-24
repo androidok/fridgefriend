8df055236bcf5baf5ffccca3cafc733415632685
pyamsoft <developer.pyamsoft@gmail.com>
2021-08-24 16:09:11 -0700

    Common vm

--- search/src/main/java/com/pyamsoft/fridge/search/SearchToolbarViewModel.kt
+++ search/src/main/java/com/pyamsoft/fridge/search/SearchToolbarViewModel.kt
@@ -16,80 +16,28 @@
 
 package com.pyamsoft.fridge.search
 
-import androidx.lifecycle.viewModelScope
-import com.pyamsoft.fridge.db.item.FridgeItem
+import com.pyamsoft.fridge.detail.BaseDetailToolbarViewModel
 import com.pyamsoft.fridge.detail.DetailListStateModel
 import com.pyamsoft.fridge.detail.DetailTopOffset
-import com.pyamsoft.fridge.detail.DetailViewState
-import com.pyamsoft.fridge.ui.view.UiToolbar
-import com.pyamsoft.pydroid.arch.Renderable
 import com.pyamsoft.pydroid.arch.UiSavedState
-import com.pyamsoft.pydroid.arch.UiSavedStateViewModel
 import com.pyamsoft.pydroid.arch.UiSavedStateViewModelProvider
-import com.pyamsoft.pydroid.arch.UnitControllerEvent
 import com.pyamsoft.pydroid.bus.EventBus
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class SearchToolbarViewModel
 @AssistedInject
 internal constructor(
-  private val delegate: DetailListStateModel,
-  private val topOffsetBus: EventBus<DetailTopOffset>,
-  @Assisted savedState: UiSavedState,
+    delegate: DetailListStateModel,
+    topOffsetBus: EventBus<DetailTopOffset>,
+    @Assisted savedState: UiSavedState,
 ) :
-    UiSavedStateViewModel<DetailViewState, UnitControllerEvent>(
+    BaseDetailToolbarViewModel(
+        delegate,
+        topOffsetBus,
         savedState,
-        initialState = delegate.initialState,
     ) {
-
-  init {
-    val scope = viewModelScope
-    val job =
-        delegate.bindState(
-            scope, Renderable { state -> state.render(scope) { scope.setState { it } } })
-    doOnCleared { job.cancel() }
-    doOnCleared { delegate.clear() }
-
-    delegate.initialize(scope)
-
-    viewModelScope.launch(context = Dispatchers.Default) {
-      val sort = restoreSavedState(KEY_SORT) { UiToolbar.SortType.CREATED_TIME }
-      handleSort(sort)
-    }
-
-    viewModelScope.launch(context = Dispatchers.Default) {
-      val search = restoreSavedState(KEY_SEARCH) { "" }
-      handleSearch(search)
-    }
-  }
-
-  fun handleSort(newSort: UiToolbar.SortType) {
-    delegate.handleUpdateSort(viewModelScope, newSort) { putSavedState(KEY_SORT, it.name) }
-  }
-
-  fun handleTopBarMeasured(height: Int) {
-    viewModelScope.launch(context = Dispatchers.Default) {
-      topOffsetBus.send(DetailTopOffset(height))
-    }
-  }
-
-  fun handleTabsSwitched(isHave: Boolean) {
-    val presence = if (isHave) FridgeItem.Presence.HAVE else FridgeItem.Presence.NEED
-    delegate.handlePresenceSwitch(viewModelScope, presence)
-  }
-
-  fun handleSearch(query: String) {
-    delegate.handleUpdateSearch(viewModelScope, query) { putSavedState(KEY_SEARCH, it) }
-  }
-
-  companion object {
-    private const val KEY_SEARCH = "search"
-    private const val KEY_SORT = "sort"
-  }
 
   @AssistedFactory
   interface Factory : UiSavedStateViewModelProvider<SearchToolbarViewModel> {
